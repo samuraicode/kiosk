918ab1cab4ebf398ad243dccd501cfc3185a374d
Jeremy Noonan <jeremy@samuraicode.com>
2016-11-13 11:11:19 -0800

    Upload

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router(),
+fs = require('fs');
+
+router.get('/', function(req, res) {
+	res.render('index', { title: 'Stand Out Talent' });
+});
+
+router.get('/kiosk/:label', function(req, res) {
+	var label = req.params.label;
+
+	res.render('index', { title: label });
+});
+
+router.get('/slides/:label', function(req, res) {
+	var label = req.params.label;
+
+	loadDirectory(label, function(result) {
+		console.log(result);
+		res.json(result);
+	});
+});
+
+function loadDirectory(directory, callback) {
+	var imageArray = [];
+
+	console.log(__dirname);
+
+	var baseDirectory = __dirname + "/../public/img/slides/" + directory + "/";
+
+	fs.readdir(baseDirectory, function(err, files) {
+		if (err) {
+			console.error(err);
+			callback(['/img/logo.png']);
+		} else {
+			files.forEach(function(f) {
+				if (f == '.DS_Store') return;
+				console.log('Adding: ' + f);
+				imageArray.push('/img/slides/' + directory + '/' + f);
+			});
+			callback(imageArray);
+		}
+	});
+}
+
+module.exports = router;
